3833e080aca025ffaccfa5c9bbeae8633122aa61
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-10-29 15:14:57 +0700

    fix multi word component name

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <h3>Essential Links</h3>
+    <h3>Ecosystem</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {
+    msg: String
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import url(../styles/home.css);
+</style>
